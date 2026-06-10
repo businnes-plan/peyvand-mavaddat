5481aa75a54956211617c8c581b13f208763732f
businnes-plan <m86599262@gmail.com>
2026-06-10 23:41:25 +0330

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ console.log("JS Loaded");
 
 const API_URL = "https://script.google.com/macros/s/AKfycbxMShJwEu4UseiCkOv97kgv08gzmtT673zUsZbZJMP2Re6DuxZkkGbRzcld1zg2hZfx/exec";
 
-
-// =====================
-// ارسال فرم
-// =====================
 async function submitForm() {
 
     const phoneRegex = /^09\d{9}$/;
@@ -23,60 +19,7 @@ async function submitForm() {
     const data = {
         gender: gender.value,
         fullName: fullName.value,
-
-        birthYear: year.value,
-        birthMonth: month.value,
-        birthDay: day.value,
-
-        height: height.value,
-        weight: weight.value,
-
-        fatherJob: fatherJob.value,
-        motherJob: motherJob.value,
-
-        skinColor: skinColor.value,
-        eyeColor: eyeColor.value,
-        dressType: dressType.value,
-
-        jobType: jobType.value,
-        jobDesc: jobDesc.value,
-
-        physicalStatus: physicalStatus.value,
-        smoking: smoking.value,
-
-        familyCount: familyCount.value,
-        childOrder: childOrder.value,
-
-        province: province.value,
-        county: county.value,
-        village: village.value,
-
-        phone1: phone1.value,
-        phone2: phone2.value,
-
-        traits: traits.value,
-        spouseCriteria: spouseCriteria.value,
-
-        marriageHistory: marriageHistory.value,
-        acceptedProvinces: acceptedProvinces.value,
-
-        jobPreference: jobPreference.value,
-        ageGap: ageGap.value,
-
-        educationMin: educationMin.value,
-        educationSelf: educationSelf.value,
-
-        income: income.value,
-        assets: assets.value,
-
-        description: description.value,
-
-        eita: eita.value,
-        rubika: rubika.value,
-        bale: bale.value,
-        soroush: soroush.value,
-
-        contactMethod: contactMethod.value
+        province: province.value
     };
 
     try {
@@ -86,37 +29,26 @@ async function submitForm() {
         });
 
         const result = await res.json();
-        alert("ثبت شد ✔️ کد: " + result.code);
+        alert("ثبت شد ✔ کد: " + result.code);
 
     } catch (err) {
         console.error(err);
-        alert("خطا در ارسال اطلاعات");
+        alert("خطا");
     }
 }
 
-
-// =====================
-// ورود ادمین
-// =====================
-function adminLogin() {
-    const pass = prompt("رمز ادمین:");
-
-    if (pass === "1234") {
-        localStorage.setItem("admin", "1");
-        alert("ورود موفق");
-    } else {
-        alert("رمز اشتباه");
+function adminLogin(){
+    const pass = prompt("رمز:");
+    if(pass === "1234"){
+        localStorage.setItem("admin","1");
+        alert("OK");
     }
 }
 
+async function loadUsers(){
 
-// =====================
-// گرفتن لیست کاربران
-// =====================
-async function loadUsers() {
-
-    if (localStorage.getItem("admin") !== "1") {
-        alert("دسترسی ندارید");
+    if(localStorage.getItem("admin") !== "1"){
+        alert("No access");
         return;
     }
 
@@ -126,12 +58,10 @@ async function loadUsers() {
     const box = document.getElementById("usersList");
     box.innerHTML = "";
 
-    users.forEach((u, i) => {
+    users.forEach((u,i)=>{
         box.innerHTML += 
-            <div style="background:#eee;padding:10px;margin:5px;border-radius:8px">
-                <b>کد:</b> ${i + 1}<br>
-                <b>جنسیت:</b> ${u.gender}<br>
-                <b>استان:</b> ${u.province}
+            <div>
+                ${u.gender} - ${u.province}
             </div>
         ;
     });
